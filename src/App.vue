b6a4dcec3c43f95fca4d728a997ae0b5f83e333f
emarti2 <eva.martin@bsc.es>
2022-02-01 21:02:24 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <ComingSoon />
+  </div>
+</template>
+
+<script>
+import ComingSoon from './components/ComingSoon.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ComingSoon
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+body{
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+main{
+  display:flex;
+  flex-direction:column;
+  width: 100%;
+}
+</style>
---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container" id="index-main">
+    <header class="row first">
+        <span class="logo"><img src="../assets/FAIRplus_logo.svg" width="20px"></span>
+        <span class='brand-name'>FAIRification  <br>Tool Discoverer</span>
+    </header>
+    <div class="row">
+      <img src="../assets/coming_soon.svg" width="25%">
+    </div>
+    <div class="row text">
+      <h2>Coming soon</h2>
+    </div>
+  </div>
+</template>
+<style scoped>
+#index-main{
+  text-align: center;
+}
+.first{
+  margin-bottom: 6em;
+  height: 30px;
+  margin-top: 4em
+}
+.brand-name{
+    font-weight: bolder;
+    color: grey;
+    text-align: left;
+    padding-left: .9em;
+    padding-top: .4em;
+    font-size: 1rem;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+  .text{
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    color: #f99768
+  }
+.logo{
+  margin-left: .7em
+}
+</style>
+<script>
+export default {
+  name: 'ComingSoon'
+}
+</script>
